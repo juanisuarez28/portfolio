56519c00b4a3f20cdc7ae679f6f6588cbad45bd5
juanisuarez38 <juanisua3@gmail.com>
2024-06-11 17:19:41 -0300

    detalle proyectos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { ProjectsComponent } from './projects/projects.component';
 import { CertificadosComponent } from './certificados/certificados.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import {MatChipsModule} from '@angular/material/chips';
+
 
 @NgModule({
   declarations: [
@@ -23,7 +25,8 @@ import { HeaderComponent } from './header/header.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    MatChipsModule
   ],
   providers: [
     provideAnimationsAsync()

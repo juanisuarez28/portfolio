85aebef95e88fb5f99fc896e7812575add81b854
juanisuarez38 <juanisua3@gmail.com>
2024-06-11 16:28:28 -0300

    new nav

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  isScrolled = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 50;
+  }
+
+  ngOnInit(): void {
+    this.onWindowScroll();
+  }
 
 }

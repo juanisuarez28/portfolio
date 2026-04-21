a2f8c5beb4f5e72d0d707b6d3ef90120fba17646
Juani Suarez <juanisua3@gmail.com>
2026-04-21 00:05:46 -0300

    fix cards mobile

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,7 +17,7 @@ export class HomeComponent implements AfterViewInit {
           observer.unobserve(entry.target);
         }
       });
-    }, { threshold: 0.1, rootMargin: '0px 0px -50px 0px' });
+    }, { threshold: 0, rootMargin: '0px 0px -50px 0px' });
 
     this.el.nativeElement.querySelectorAll('.reveal').forEach((elem: any) => {
       observer.observe(elem);

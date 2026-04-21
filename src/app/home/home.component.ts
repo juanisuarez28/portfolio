5fd61daa74b72ccca15f68a911b1d62886f0e205
Juani Suarez <juanisua3@gmail.com>
2026-04-20 23:43:30 -0300

    Actualiza portfolio con nuevo diseño y proyectos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  
+  constructor(private el: ElementRef) {}
 
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('active');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, { threshold: 0.1, rootMargin: '0px 0px -50px 0px' });
+
+    this.el.nativeElement.querySelectorAll('.reveal').forEach((elem: any) => {
+      observer.observe(elem);
+    });
+  }
 }
